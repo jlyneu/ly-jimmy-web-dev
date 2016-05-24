37b277823f1bd1c857b39d7b1a43d01b0553cc07
jlyneu <jly@ccs.neu.edu>
2016-05-24 02:15:20 -0400

    Populate page-list pages

--- public/assignment/views/page/page.controller.client.js
+++ public/assignment/views/page/page.controller.client.js
@@ -5,8 +5,14 @@
         .controller("NewPageController", NewPageController)
         .controller("EditPageController", EditPageController);
 
-    function PageListController() {
+    function PageListController($routeParams, PageService) {
         var vm = this;
+        vm.userId = $routeParams["uid"];
+        vm.websiteId = $routeParams["wid"];
+        function init() {
+            vm.pages = PageService.findPagesByWebsiteId(vm.websiteId);
+        }
+        init();
     }
 
     function NewPageController() {
