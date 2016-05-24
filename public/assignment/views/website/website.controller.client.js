2f39159589f9bd719fae701ab5b77f617f2dfad6
jlyneu <jly@ccs.neu.edu>
2016-05-24 19:04:02 -0400

    Adding event handlers to website-edit

--- public/assignment/views/website/website.controller.client.js
+++ public/assignment/views/website/website.controller.client.js
@@ -20,7 +20,23 @@
 
     function EditWebsiteController($routeParams, WebsiteService) {
         var vm = this;
+        vm.userId = $routeParams["uid"];
         vm.websiteId = $routeParams["wid"];
+        
+        // event handler declarations
+        vm.updateWebsite = updateWebsite;
+        vm.deleteWebsite = deleteWebsite;
+        
+        // event handler functions
+        function updateWebsite(website) {
+            WebsiteService.updateWebsite(vm.websiteId, vm.website);
+        }
+        
+        function deleteWebsite() {
+            WebsiteService.deleteWebsite(vm.websiteId);
+        }
+        
+        // initialization to populate form fields
         function init() {
             vm.website = WebsiteService.findWebsiteById(vm.websiteId);
         }
