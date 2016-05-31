68bff6d14f87963c52dd83189e015f8bc4d61464
jlyneu <jly@ccs.neu.edu>
2016-05-31 19:59:32 -0400

    Use json instead of send when possible

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -29,7 +29,7 @@ module.exports = function(app) {
         } else if (username) {
             findUserByUsername(username, res);
         } else {
-            res.send(users);
+            res.json(users);
         }
     }
 
