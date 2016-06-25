2c5fc1b419064067c4e43c93892fb397ddc2a70c
jlyneu <jly@ccs.neu.edu>
2016-06-25 04:20:11 -0400

    Add logout, pass user to header directive

--- public/project/views/profile/profile-detail.controller.client.js
+++ public/project/views/profile/profile-detail.controller.client.js
@@ -4,8 +4,29 @@
         .controller("ProfileDetailController", ProfileDetailController);
 
     // controller for the profile-detail.view.client.html template
-    function ProfileDetailController($rootScope) {
+    function ProfileDetailController($rootScope, $location) {
         var vm = this;
+
+        vm.logout = logout;
+
         vm.user = $rootScope.currentUser;
+
+        // make a request to the server to logout the current user. if successful,
+        // route the user to the login page. also clear the current user from the rootScope.
+        // otherwise, an error occurred so display an error.
+        function logout() {
+            UserService
+                .logout()
+                .then(logoutSuccess, logoutError);
+
+            function logoutSuccess(response) {
+                $rootScope.currentUser = null;
+                $location.url("/");
+            }
+
+            function logoutError(error) {
+                vm.error = "Could not logout. Please try again later.";
+            }
+        }
     }
 })();
