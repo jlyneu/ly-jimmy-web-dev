06b8d2e6cd0809cd46cdad2dcf655892bd50cb89
jlyneu <jly@ccs.neu.edu>
2016-06-25 06:00:44 -0400

    Added pet list to shelter detail

--- public/project/views/shelter/shelter-detail.controller.client.js
+++ public/project/views/shelter/shelter-detail.controller.client.js
@@ -4,7 +4,7 @@
         .controller("ShelterDetailController", ShelterDetailController);
 
     // controller for the shelter-list.view.client.html template
-    function ShelterDetailController($rootScope, $routeParams, ShelterService) {
+    function ShelterDetailController($rootScope, $routeParams, ShelterService, PetService) {
         var vm = this;
 
         vm.shelterId = $routeParams["shelterId"];
@@ -15,16 +15,28 @@
                 .findShelterById(vm.shelterId)
                 .then(findShelterByIdSuccess, findShelterByIdError);
 
+
             function findShelterByIdSuccess(response) {
                 vm.shelter = response.data;
                 for (var i = 0; i < vm.shelter.users.length; i++) {
                     if (vm.shelter.users[i] === vm.user._id) {
                         vm.isOwner = true;
+                        return PetService
+                            .findPetsByShelterId(vm.shelterId)
+                            .then(findPetsByShelterIdSuccess, findPetsByShelterIdError);
                     }
                 }
             }
 
             function findShelterByIdError(error) {
+
+            }
+
+            function findPetsByShelterIdSuccess(response) {
+                vm.pets = response.data;
+            }
+
+            function findPetsByShelterIdError(error) {
 
             }
         }
