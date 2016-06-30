63b09d1184c8f618fc74a8fa2094ead937ff5863
jlyneu <jly@ccs.neu.edu>
2016-06-30 03:07:51 -0400

    Fixing add pet issue with shelter detail

--- public/project/views/shelter/shelter-detail.controller.client.js
+++ public/project/views/shelter/shelter-detail.controller.client.js
@@ -59,8 +59,10 @@
                         }
                     }
                     if (vm.shelter.users) {
+                        console.log(vm.shelter.users);
+                        console.log(vm.user._id);
                         for (i = 0; i < vm.shelter.users.length; i++) {
-                            if (vm.shelter.users[i] === vm.user._id) {
+                            if (vm.shelter.users[i]._id === vm.user._id) {
                                 vm.isOwner = true;
                             }
                         }
