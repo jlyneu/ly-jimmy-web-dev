ed1b1a82b9449119192fedc35fa023a1904b692a
jlyneu <jly@ccs.neu.edu>
2016-07-01 02:43:08 -0400

    Clean up messagethread list

--- public/project/views/messagethread/messagethread-list.controller.client.js
+++ public/project/views/messagethread/messagethread-list.controller.client.js
@@ -7,23 +7,27 @@
     function MessagethreadListController($rootScope, MessagethreadService) {
         var vm = this;
         
+        // get current user from rootScope if present
         vm.user = $rootScope.currentUser;
 
+        // intialize messagethread list page by finding message threads for current user
         function init() {
             MessagethreadService
                 .findMessagethreadsByUserId(vm.user._id)
                 .then(findMessagethreadsByUserIdSuccess, findMessagethreadsByUserIdError);
-
-
+            
+            // a 200 came back. if messagethreads returned arent null, then set messagethread data and sort
+            // by last message time in each messagethread
             function findMessagethreadsByUserIdSuccess(response) {
                 vm.messagethreads = response.data;
+                // sort messages by last updated time
                 vm.messagethreads.sort(function(a, b) {
                     return new Date(b.dateUpdated) - new Date(a.dateUpdated);
                 })
             }
 
             function findMessagethreadsByUserIdError(error) {
-
+                vm.error = "Could not fetch messages at this time. Please try again later.";
             }
         }
         init();
